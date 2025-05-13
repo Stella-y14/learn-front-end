6fb664d84cf3d693ccfed939249eb2668576a0f7
Dain <stella@go.byuh.edu>
2025-05-12 23:18:50 -1000

    first React

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -4,14 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>
-          Hello World
-        </h1>
-        <p>Welcome</p>
-        <p>Changes are implemented immediately</p>
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <h1>
+        Hello World
+      </h1>
+      <p>Welcome</p>
+      <p>Changes are implemented immediately</p>
+      <img src={logo} className="App-logo" alt="logo" />
     </div>
   );
 }
