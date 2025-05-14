add8ed1438bc458f3828332a935217002e075828
Dain <stella@go.byuh.edu>
2025-05-13 21:42:29 -1000

    some features in React

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -4,13 +4,65 @@ import ReactDOM from 'react-dom/client';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
-const myFirstelement = <h1>Hello React!</h1>
+//if statements
+const x = 5;
+let text = "Goodbye";
+if (x < 10) {
+  text = "Hello";
+}
+
+function Car(props) {
+  return <h2>I am a {props.color} Car!</h2>;
+}
+
+function Garage() {
+  return (
+    <>
+      <h1>Who lives in my Garage?</h1>
+      <Car color="red"/>
+    </>
+  );
+}
+
+// function Football() {
+//   const shoot = () => {
+//     alert("Great Shot!");
+//   }
+
+//   return (
+//     <button onClick={shoot}>Take the shot!</button>
+//   );
+// }
+function Football() {
+  const shoot = (a) => {
+    alert(a);
+  }
+
+  return (
+    <button onClick={() => shoot("Goal!")}>Take the shot!</button>
+  );
+}
+
+const myFirstelement = (
+  //JSX
+    <>
+    <h1 className="myclass">Hello React!{5 + 5}</h1>
+    <ul>
+      <li>Apples</li>
+      <li>Bananas</li>
+      <li>Cherries</li>
+    </ul>
+    <input type="text" />
+    <h1>{text}</h1>
+    {/* another expression of if */}
+    <h1>{(x) < 10 ? "Hello" : "Goodbye"}</h1> 
+    <Garage />
+    <Football />
+    </>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
   myFirstelement
 );
 
