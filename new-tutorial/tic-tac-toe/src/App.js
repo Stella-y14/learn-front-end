1cb8063c5a1acd023f6b672805b0722993fa5713
Dain <stella@go.byuh.edu>
2025-05-20 19:36:35 -1000

    get winner

--- new-tutorial/tic-tac-toe/src/App.js
+++ new-tutorial/tic-tac-toe/src/App.js
@@ -21,7 +21,7 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
-    if (squares[i]) {
+    if (squares[i] || Winner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
@@ -35,7 +35,16 @@ export default function Board() {
     setXIsNext(!xIsNext);
   }
 
+  const winr = Winner(squares);
+  let status;
+  if (winr) {
+    status = "WInner : " + winr;
+  } else {
+    status = "Next player : " + (xIsNext ? "X" : "O");
+  }
+
   return (<>
+  <div className="status">{status}</div>
     <div className="board-row">
       <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
       <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -52,4 +61,24 @@ export default function Board() {
       <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
     </div>  
   </>);
+}
+
+function Winner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
