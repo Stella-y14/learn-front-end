c7635dce2faff45c36de260a8bfa657e41fc8ca2
Dain <stella@go.byuh.edu>
2025-05-27 18:58:49 -1000

    get items in the list

--- learn-front-end3/src/App.jsx
+++ learn-front-end3/src/App.jsx
@@ -4,11 +4,25 @@ import "./styles.css"
 //return only one element. so need to wrap inside a fragment
 export default function App() {
   const [newItem, setNewItem] = useState("") // [value, function]
+  const [todos, setTodos] = useState([])
   
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos, 
+      { id: crypto.randomUUID(), 
+        title: newItem, completed:false },
+    ]
+  })
+}
+
+  console.log(todos)
 
   return (
     <>
-    <form className="new-iten-form">
+    <form onSubmit={handleSubmit} className="new-iten-form">
     <div className="form-row">
       <label htmlFor="item">New Item</label>
       <input 
@@ -21,13 +35,18 @@ export default function App() {
     </form>
     <h1 className="header">Todo List</h1>
     <ul className="list">
-      <li>
-        <label>
-          <input type="checkbox" />
-          item1
-        </label>
-        <button className="bun  btn-danger">Delete</button>
-      </li>
+      {todos.map(todo =>  {
+        return (
+          <li>
+            <label>
+              <input type="checkbox" checked={todo.completed} />
+              {todo.title}
+            </label>
+            <button className="bun  btn-danger">Delete</button>
+          </li>
+        )
+      })} 
+
     </ul>
   </>
   )
