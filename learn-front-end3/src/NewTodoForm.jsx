ab9dba07fdc412e3f1830863f49eac33d93ee882
Dain <stella@go.byuh.edu>
2025-05-27 21:55:11 -1000

    text area

--- learn-front-end3/src/NewTodoForm.jsx
+++ learn-front-end3/src/NewTodoForm.jsx
@@ -16,10 +16,11 @@ export function NewTodoForm({ onSubmit }) {
     <form onSubmit={handleSubmit} className="new-item-form">
         <div className="form-row">
             <label htmlFor="item">New Item</label>
-            <input 
-            value={newItem} 
-            onChange={e => setNewItem(e.target.value)} 
-            type="text" id="item" 
+            <textarea 
+                value={newItem} 
+                onChange={e => setNewItem(e.target.value)} 
+                rows={4}
+                type="text" id="item" 
         />
         </div>
         <button className="btn">Add</button>
