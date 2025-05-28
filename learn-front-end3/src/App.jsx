5295719da4a7dffab6c9376c33e2a2f66c50b08d
Dain <stella@go.byuh.edu>
2025-05-27 19:10:37 -1000

    add delete function

--- learn-front-end3/src/App.jsx
+++ learn-front-end3/src/App.jsx
@@ -31,6 +31,12 @@ function toggleTodo(id, completed) {
   })
 }
 
+function deleteTodo(id) {
+  setTodos(currentTodos => {
+    return currentTodos.filter(todo => todo.id == id)
+  })
+}
+
   return (
     <>
     <form onSubmit={handleSubmit} className="new-iten-form">
@@ -46,15 +52,22 @@ function toggleTodo(id, completed) {
     </form>
     <h1 className="header">Todo List</h1>
     <ul className="list">
+      {todos.length === 0 && "No Todos"}
       {todos.map(todo =>  {
         return (
           <li key={todo.id}>
             <label>
               <input type="checkbox" checked={todo.completed}
-              onChange={e => toggleTodo(todo.id, e.target.checked)} />
+              onChange={e => toggleTodo(todo.id, e.target.checked)} 
+              />
               {todo.title}
             </label>
-            <button className="bun  btn-danger">Delete</button>
+            <button 
+              onclick={() => deleteTodo(todo.id)} 
+              className="bun  btn-danger"
+            >
+              Delete
+              </button>
           </li>
         )
       })} 
