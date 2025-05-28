2ef8effd31e68c141865ab34717bcb0310b2b32a
Dain <stella@go.byuh.edu>
2025-05-27 18:36:19 -1000

    construct basic JSX

--- learn-front-end3/src/App.jsx
+++ learn-front-end3/src/App.jsx
@@ -1,10 +1,26 @@
 import "./styles.css"
 
+//return only one element. so need to wrap inside a fragment
 export default function App() {
-  return <form className="new-iten-form">
+  return (
+    <>
+    <form className="new-iten-form">
     <div className="form-row">
       <label htmlFor="item">New Item</label>
       <input type="text" id="item" />
     </div>
-  </form>
+    <button className="btn">Add</button>
+    </form>
+    <h1 className="header">Todo List</h1>
+    <ul className="list">
+      <li>
+        <label>
+          <input type="checkbox" />
+          item1
+        </label>
+        <button className="bun  btn-danger">Delete</button>
+      </li>
+    </ul>
+  </>
+  )
 }
