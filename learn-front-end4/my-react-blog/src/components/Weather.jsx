133be12a6fdcdb272340869040b28106d69b393e
Dain <stella@go.byuh.edu>
2025-06-09 23:19:58 -1000

    set weather jsx

--- learn-front-end4/my-react-blog/src/components/Weather.jsx
+++ learn-front-end4/my-react-blog/src/components/Weather.jsx
@@ -3,7 +3,21 @@ import './Weather.css'
 
 const Weather = () => {
   return (
-    <div className='weather'>Weather</div>
+    <div className='weather'>
+      <div className="search">
+        <div className="serach-top">
+          <i className="fa-solid fa-location-dot"></i>
+          <div className="location">Tbilisi</div>
+        </div>
+        <div className="search-location">
+          <input type="text" placeholer='Enter Location' />
+          <i className="fa-solid fa-magnifying-glass"></i>
+        </div>
+      </div>
+      <div className="weather-date">
+        <i className='bx bxs-sun' ></i> 
+      </div>
+    </div>
   )
 }
 
