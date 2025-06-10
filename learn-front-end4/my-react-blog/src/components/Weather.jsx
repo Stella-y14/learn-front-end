de7c0b1e77f5443af5ba370c7243df853394b891
Dain <stella@go.byuh.edu>
2025-06-09 23:53:17 -1000

    weather css

--- learn-front-end4/my-react-blog/src/components/Weather.jsx
+++ learn-front-end4/my-react-blog/src/components/Weather.jsx
@@ -5,20 +5,22 @@ const Weather = () => {
   return (
     <div className='weather'>
       <div className="search">
-        <div className="serach-top">
+        <div className="search-top">
           <i className="fa-solid fa-location-dot"></i>
           <div className="location">Tbilisi</div>
         </div>
         <div className="search-location">
-          <input type="text" placeholer='Enter Location' />
+          <input type="text" placeholder='Enter Location' />
           <i className="fa-solid fa-magnifying-glass"></i>
         </div>
       </div>
       <div className="weather-data">
         <i className='bx bxs-sun' ></i> 
+        <div className="weather-type">Clear</div>
+        <div className="temp">28º</div>
       </div>
     </div>
   )
 }
 
-export default Weather
+export default Weather 
